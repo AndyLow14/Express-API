8c336b71605eabbaf74e545ea38201152ba1e6b0
Andy Low <andyjunchenglow@gmail.com>
2022-08-29 19:09:58 +1000

    Added endpoint handling

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log(`it's alive on http://localhost:${PORT}`)
-)
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: 'Pokemon',
+    size: 'Large'
+  })
+});
+
+app.post("/tshirt/:id", (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        req.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `Tshirt with your ${logo} and ID of ${id}`,
+    })
+
+})
